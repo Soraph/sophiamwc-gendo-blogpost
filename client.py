0705e58d553962f4f344a7d701f5793701fb1c2c
Matteo Canu <matteo@soraph.me>
2017-04-13 16:50:32 +0200

    Added remote call to add a new post on the server.

--- client.py
+++ client.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python
+import requests
+
+
+def main():
+    r = requests.post('http://127.0.0.1:5000/', {
+            'title': 'Post from client',
+            'content': 'Lorem ipsum'
+        })
+    print(r.text)
+
+
+if __name__ == '__main__':
+    main()
