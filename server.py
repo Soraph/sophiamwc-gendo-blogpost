901fa75d812a36e6e9941e3bb5b86c32465ce8fe
Matteo Canu <matteo@soraph.me>
2017-04-13 15:49:50 +0200

    Renamed home to homepage and moved the instructions to the GET method only.

--- server.py
+++ server.py
@@ -4,30 +4,31 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 db = sqlite3.connect('blog.db', check_same_thread=False)
+db.row_factory = sqlite3.Row
 c = db.cursor()
 
 
-@app.route('/', endpoint='home', methods=['GET', 'POST'])
-def home():
+@app.route('/', endpoint='homepage', methods=['GET', 'POST'])
+def homepage():
     if request.method == 'POST':
         pass
+    else:
+        sql_all_posts = '''
+            SELECT
+                id, title, content
+            FROM
+                posts
+            ORDER BY
+                id DESC
+        '''
+        c.execute(sql_all_posts)
 
-    sql_all_posts = '''
-        SELECT
-            id, title, content
-        FROM
-            posts
-        ORDER BY
-            id DESC
-    '''
-    c.execute(sql_all_posts)
+        page_data = {
+                'page_title': 'Homepage',
+                'posts': c.fetchall()
+        }
 
-    page_data = {
-            'page_title': 'Homepage',
-            'posts': c.fetchall()
-    }
-
-    return render_template('base--homepage.html', **page_data)
+        return render_template('base--homepage.html', **page_data)
 
 
 if __name__ == '__main__':
